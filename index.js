c36d927a0f53593aa990dc40182575f9b805a314
farste <sfargali@gmail.com>
2019-10-16 19:15:04 -0400

    added enpoints to encode and decode into base62

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ const server = express();
 const cors = require("cors");
 server.use(cors());
 server.use(express.json());
+var base62 = require("base62/lib/ascii");
 
 server.get('/', (req, res) => {
     res.send('I live');
+})
+
+server.post('/base62', (req, res) => {
+    const number = req.body.number;
+    result = base62.encode(number);
+    res.send(`${result}`);
+});
+
+server.post('/decrypt', (req, res) => {
+    const target = req.body.target;
+    result = base62.decode(target);
+    res.send(`${result}`);
 })
 
 const port = process.env.PORT || 3300;
