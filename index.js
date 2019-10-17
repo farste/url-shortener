fe452dbc41cda23f669bb63befe731f1d05956e9
farste <sfargali@gmail.com>
2019-10-17 15:21:22 -0400

    hash endpoint takes url and returns base62 string

--- index.js
+++ index.js
@@ -6,8 +6,30 @@ server.use(cors());
 server.use(express.json());
 var base62 = require("base62/lib/ascii");
 
+//Hash function
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    hash = Math.abs(hash);
+    return hash;
+}
+
 server.get('/', (req, res) => {
     res.send('I live');
+});
+
+server.post('/hash', (req, res) => {
+    const string = req.body.string;
+    hash = string.hashCode();
+    result = base62.encode(hash);
+    res.send(`${hash} \n ${result}`);
 });
 
 server.post('/base62', (req, res) => {
